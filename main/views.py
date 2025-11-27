6e472473347b030428168ce613a400468bae09e4
Dmytro Semenov <comagent5@gmail.com>
2025-11-27 13:04:23 +0200

    First commit with example template

---
+++ main/views.py
@@ -0,0 +1,20 @@
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def index(request: HttpRequest) -> HttpResponse:
+    content = {
+        'title': 'Мій перший WEB додаток',
+        'h1': 'Головна сторінка',
+        'content': 'Контент головної сторінки',
+        'list': ['one', 'two', 'three'],
+        'dict': {
+            1: 'one',
+            2: 'two'},
+        'bool': True,
+    }
+    return render(request, 'main/index.html', content) 
+
+
+def about(request: HttpRequest) -> HttpResponse:
+    return HttpResponse('About Page')
